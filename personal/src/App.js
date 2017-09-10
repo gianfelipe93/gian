418e63e809753c11e076665bb77a6efe68faacae
Gian Felipe <gian.guerra@squidit.com.br>
2017-09-10 15:33:51 +1000

    :construction: add text on the right side

--- personal/src/App.js
+++ personal/src/App.js
@@ -26,6 +26,20 @@ class App extends Component {
                     <div className="my-picture">
                         <img src={me} className="me"/>
             </div>
+            <div className="main-text">
+                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id turpis vitae massa efficitur sodales.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id turpis vitae massa efficitur sodales. Donec faucibus sapien tortor, et imperdiet risus iaculis eget. Proin volutpat ex in enim dictum, at molestie est finibus. Phasellus eu luctus justo, vel facilisis lectus. Sed in tortor et urna pulvinar tincidunt. Curabitur aliquet ac purus sit amet convallis. Aenean feugiat diam odio, quis egestas velit accumsan ut. Quisque non pulvinar turpis. In a eleifend ante.
+                </p>
+            <p>
+                Suspendisse convallis, nulla ut sodales interdum, elit tellus bibendum tellus, sollicitudin efficitur metus dui a dui. Pellentesque consectetur accumsan est, fermentum consequat turpis condimentum tempus. Fusce tempus vestibulum dui ac vehicula. Curabitur porttitor blandit ipsum sit amet porttitor. Sed pellentesque ornare purus sed ornare. In non volutpat orci. Quisque at urna sed elit dignissim interdum. Morbi volutpat purus a diam faucibus, a lobortis nisl accumsan. Aliquam at dapibus est. Duis quis tortor sed arcu fringilla ultrices at eu tellus. Vivamus pulvinar ullamcorper tincidunt. Cras aliquam eleifend quam eu facilisis. 
+            </p>
+            <p>
+                Suspendisse convallis, nulla ut sodales interdum, elit tellus bibendum tellus, sollicitudin efficitur metus dui a dui. Pellentesque consectetur accumsan est, fermentum consequat turpis condimentum tempus. Fusce tempus vestibulum dui ac vehicula. Curabitur porttitor blandit ipsum sit amet porttitor. Sed pellentesque ornare purus sed ornare. In non volutpat orci. Quisque at urna sed elit dignissim interdum. Morbi volutpat purus a diam faucibus, a lobortis nisl accumsan. Aliquam at dapibus est. Duis quis tortor sed arcu fringilla ultrices at eu tellus. Vivamus pulvinar ullamcorper tincidunt. Cras aliquam eleifend quam eu facilisis. 
+            </p>
+            <p>
+                Suspendisse convallis, nulla ut sodales interdum, elit tellus bibendum tellus, sollicitudin efficitur metus dui a dui. Pellentesque consectetur accumsan est, fermentum consequat turpis condimentum tempus. Fusce tempus vestibulum dui ac vehicula. Curabitur porttitor blandit ipsum sit amet porttitor. Sed pellentesque ornare purus sed ornare. In non volutpat orci. Quisque at urna sed elit dignissim interdum. Morbi volutpat purus a diam faucibus, a lobortis nisl accumsan. Aliquam at dapibus est. Duis quis tortor sed arcu fringilla ultrices at eu tellus. Vivamus pulvinar ullamcorper tincidunt. Cras aliquam eleifend quam eu facilisis. 
+            </p>
+            </div>
             </div>
             </div>
             </div>
